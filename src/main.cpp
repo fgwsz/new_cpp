6da356e7ae5fc34372fbe3be8946523eb86e5a35
fgwsz <3191000458@qq.com>
2023-03-22 17:31:17 +0800

    update type.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
 #include"reflect/meta.h"
+#include"reflect/type.h"
 int main(void) {
-	::std::cout << typeid(TypeList<
-		StaticAutoValue<true>,
-		StaticAutoValue<100,200>,
-		StaticAutoValue<3.14,2.2>,
-		STATIC_STRING("hello world")
-	>).name() << ::std::endl;
-	::std::cout << typeid(STATIC_STRING("time")::value_).name() << ::std::endl;
+	::std::cout << TypeInfo<int(int[], char)>::string() << ::std::endl;
 	return 0;
 }
--- src/reflect/type.h
+++ src/reflect/type.h
@@ -4,107 +4,117 @@
 using StringType = ::std::string;
 template<typename _Type>
 struct TypeInfo{
-	static auto string() const { 
+	static auto string() { 
 		return StringType{ typeid(_Type).name() }; 
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type const> {
-	static auto string() const {
+	static auto string() {
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const}"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type volatile> {
-	static auto string() const {
+	static auto string() {
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" volatile}"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type const volatile> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const volatile}"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type *> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" *"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type &> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" &"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type &&> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" &&"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type[]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" []"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type const[]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const []"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type volatile[]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" volatile []"};
 	}
 };
 template<typename _Type>
 struct TypeInfo<_Type const volatile[]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const volatile []"};
 	}
 };
 template<typename _Type,SizeType _N>
 struct TypeInfo<_Type[_N]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
 			StringType{ " [" } + ::std::to_string(_N) + StringType{"]"};
 	}
 };
 template<typename _Type,SizeType _N>
 struct TypeInfo<_Type const[_N]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
 			StringType{ " const [" } + ::std::to_string(_N) + StringType{"]"};
 	}
 };
 template<typename _Type,SizeType _N>
 struct TypeInfo<_Type volatile[_N]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
 			StringType{ " volatile [" } + ::std::to_string(_N) + StringType{"]"};
 	}
 };
 template<typename _Type,SizeType _N>
 struct TypeInfo<_Type const volatile[_N]> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
 			StringType{ " const volatile [" } + ::std::to_string(_N) + StringType{"]"};
 	}
 };
 template<typename _ClassType,typename _Type>
 struct TypeInfo<_Type _ClassType::*> {
-	static auto string() const {
+	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
 			StringType{ "} " } + TypeInfo<_Type>::string() + StringType{" ::*"};
 	}
 };
 template<typename _RetType,typename..._Types>
 struct TypeInfo<_RetType(_Types...)> {
+	static auto string() { 
+		if constexpr (sizeof...(_Types) != 0) {
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + 
+				StringType{ "}(" } + (TypeInfo<_Types>::string().append(",") + ...) + StringType{")"};
+		}
+		else {
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + 
+				StringType{ "}()" };
+		}
+	}
 };
 template<typename _RetType,typename..._Types>
 struct TypeInfo<_RetType(_Types...)const> {
