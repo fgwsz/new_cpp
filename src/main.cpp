71b577f4e2f5bc6598ee018022a53f4457b6ea8c
fgwsz <3191000458@qq.com>
2023-03-23 17:35:03 +0800

    impl type info string

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
-#include"reflect/meta.h"
 #include"reflect/type.h"
+class Class {
+public:
+	int func(int&&, ...)const volatile&& noexcept;
+	static int func2(int&&, ...)noexcept;
+};
 int main(void) {
-	::std::cout << TypeInfo<int(int[], char)>::string() << ::std::endl;
+	::std::cout << TypeInfo<decltype(& Class::func)>::string() << ::std::endl;
+	::std::cout << TypeInfo<decltype(Class::func2)>::string() << ::std::endl;
+	::std::cout << TypeInfo<decltype(& Class::func2)>::string() << ::std::endl;
+	::std::cout << TypeInfo<decltype(& ::std::string::push_back)>::string() << ::std::endl;
 	return 0;
 }
--- src/reflect/type.h
+++ src/reflect/type.h
@@ -1,260 +1,119 @@
 #pragma once
-#include"meta.h"
 #include<string>
 using StringType = ::std::string;
+using SizeType = unsigned long long;
 template<typename _Type>
-struct TypeInfo{
-	static auto string() { 
-		return StringType{ typeid(_Type).name() }; 
+struct TypeInfo {
+private:
+	static constexpr StringType typeName() {
+		StringType ret{ __FUNCTION__ };
+		auto begin_index = ret.find_first_of('<') + 1;
+		auto end_index = ret.find_last_of('>');
+		return ret.substr(begin_index, end_index - begin_index);
+	}
+public:
+	static constexpr auto string() { 
+		return TypeInfo<_Type>::typeName();
 	}
 };
-template<typename _Type>
-struct TypeInfo<_Type const> {
-	static auto string() {
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const}"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type volatile> {
-	static auto string() {
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" volatile}"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type const volatile> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const volatile}"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type *> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" *"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type &> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" &"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type &&> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" &&"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type[]> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" []"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type const[]> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const []"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type volatile[]> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" volatile []"};
-	}
-};
-template<typename _Type>
-struct TypeInfo<_Type const volatile[]> {
-	static auto string() { 
-		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{" const volatile []"};
-	}
-};
-template<typename _Type,SizeType _N>
-struct TypeInfo<_Type[_N]> {
-	static auto string() { 
-		return StringType{ "{" } + TypeInfo<_Type>::string() + 
-			StringType{ " [" } + ::std::to_string(_N) + StringType{"]"};
-	}
-};
-template<typename _Type,SizeType _N>
-struct TypeInfo<_Type const[_N]> {
-	static auto string() { 
-		return StringType{ "{" } + TypeInfo<_Type>::string() + 
-			StringType{ " const [" } + ::std::to_string(_N) + StringType{"]"};
-	}
-};
-template<typename _Type,SizeType _N>
-struct TypeInfo<_Type volatile[_N]> {
-	static auto string() { 
-		return StringType{ "{" } + TypeInfo<_Type>::string() + 
-			StringType{ " volatile [" } + ::std::to_string(_N) + StringType{"]"};
-	}
-};
-template<typename _Type,SizeType _N>
-struct TypeInfo<_Type const volatile[_N]> {
-	static auto string() { 
-		return StringType{ "{" } + TypeInfo<_Type>::string() + 
-			StringType{ " const volatile [" } + ::std::to_string(_N) + StringType{"]"};
-	}
-};
+#define _TYPE_INFO_BASE_IMPL(__EXT__) \
+template<typename _Type> \
+struct TypeInfo<_Type __EXT__> { \
+	static auto string() { \
+		return StringType{"{"} + TypeInfo<_Type>::string() + StringType{"}"} + StringType{ #__EXT__ }; \
+	} \
+}; \
+//
+#define _TYPE_INFO_ARRAY_IMPL(__EXT__) \
+template<typename _Type,SizeType _N> \
+struct TypeInfo<_Type __EXT__[_N]> { \
+	static auto string() { \
+		return StringType{ "{" } + TypeInfo<_Type>::string() + \
+			StringType{ "}"} + StringType{ #__EXT__ } + \
+            StringType{ "[" } + ::std::to_string(_N) + StringType{"]"}; \
+	} \
+}; \
+//
 template<typename _ClassType,typename _Type>
 struct TypeInfo<_Type _ClassType::*> {
 	static auto string() { 
 		return StringType{ "{" } + TypeInfo<_Type>::string() + 
-			StringType{ "} " } + TypeInfo<_Type>::string() + StringType{" ::*"};
+			StringType{ "}{" } + TypeInfo<_ClassType>::string() + StringType{"}::*"};
 	}
 };
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)> {
-	static auto string() { 
-		if constexpr (sizeof...(_Types) != 0) {
-			return StringType{ "{" } + TypeInfo<_RetType>::string() + 
-				StringType{ "}(" } + (TypeInfo<_Types>::string().append(",") + ...) + StringType{")"};
-		}
-		else {
-			return StringType{ "{" } + TypeInfo<_RetType>::string() + 
-				StringType{ "}()" };
-		}
-	}
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile&&> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile&&> {
-};
-
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)volatile&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...)const volatile&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...) noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)volatile&& noexcept> {
-};
-template<typename _RetType,typename..._Types>
-struct TypeInfo<_RetType(_Types...,...)const volatile&& noexcept> {
-};
+#define _TYPE_INFO_FUNCTION_IMPL(__EXT__) \
+template<typename _RetType,typename..._Types> \
+struct TypeInfo<_RetType(_Types...)__EXT__> { \
+	static auto string() { \
+		if constexpr (sizeof...(_Types) != 0) { \
+			StringType arg_list = (TypeInfo<_Types>::string().append(StringType{ "," }) + ...); \
+            arg_list.pop_back(); \
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + \
+				StringType{ "}(" } + arg_list + StringType{ ")" } + StringType{ #__EXT__ }; \
+		} \
+		else { \
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + \
+				StringType{ "}(void)" } + StringType{ #__EXT__ }; \
+		} \
+	} \
+}; \
+template<typename _RetType,typename..._Types> \
+struct TypeInfo<_RetType(_Types...,...)__EXT__> { \
+	static auto string() { \
+		if constexpr (sizeof...(_Types) != 0) { \
+			; \
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + \
+				StringType{ "}(" } + (TypeInfo<_Types>::string().append(StringType{ "," }) + ...) + \
+                StringType{ "...)" } + StringType{ #__EXT__ }; \
+		} \
+		else { \
+			return StringType{ "{" } + TypeInfo<_RetType>::string() + \
+				StringType{ "}(...)" } + StringType{ #__EXT__ }; \
+		} \
+	} \
+}; \
+//
+_TYPE_INFO_BASE_IMPL(const)
+_TYPE_INFO_BASE_IMPL(volatile)
+_TYPE_INFO_BASE_IMPL(const volatile)
+_TYPE_INFO_BASE_IMPL(*)
+_TYPE_INFO_BASE_IMPL(&)
+_TYPE_INFO_BASE_IMPL(&&)
+_TYPE_INFO_BASE_IMPL([])
+_TYPE_INFO_BASE_IMPL(const[])
+_TYPE_INFO_BASE_IMPL(volatile[])
+_TYPE_INFO_BASE_IMPL(const volatile[])
+#undef _TYPE_INFO_BASE_IMPL
+// C Style Array[_N]
+_TYPE_INFO_ARRAY_IMPL() 
+_TYPE_INFO_ARRAY_IMPL(const) 
+_TYPE_INFO_ARRAY_IMPL(volatile) 
+_TYPE_INFO_ARRAY_IMPL(const volatile) 
+#undef _TYPE_INFO_ARRAY_IMPL
+//
+_TYPE_INFO_FUNCTION_IMPL()
+_TYPE_INFO_FUNCTION_IMPL(const)
+_TYPE_INFO_FUNCTION_IMPL(volatile)
+_TYPE_INFO_FUNCTION_IMPL(const volatile)
+_TYPE_INFO_FUNCTION_IMPL(&)
+_TYPE_INFO_FUNCTION_IMPL(const&)
+_TYPE_INFO_FUNCTION_IMPL(volatile&)
+_TYPE_INFO_FUNCTION_IMPL(const volatile&)
+_TYPE_INFO_FUNCTION_IMPL(&&)
+_TYPE_INFO_FUNCTION_IMPL(const&&)
+_TYPE_INFO_FUNCTION_IMPL(volatile&&)
+_TYPE_INFO_FUNCTION_IMPL(const volatile&&)
+// noexcept
+_TYPE_INFO_FUNCTION_IMPL(noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const noexcept)
+_TYPE_INFO_FUNCTION_IMPL(volatile noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const volatile noexcept)
+_TYPE_INFO_FUNCTION_IMPL(&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(volatile&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const volatile&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(&&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const&&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(volatile&&noexcept)
+_TYPE_INFO_FUNCTION_IMPL(const volatile&&noexcept)
+#undef _TYPE_INFO_FUNCTION_IMPL
